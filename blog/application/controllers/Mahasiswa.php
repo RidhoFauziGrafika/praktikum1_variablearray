d55e6f68ad14f1c5c6931547dedc90a5e6d4741d
RidhoFauziGrafika <ridh21142ti@student.nurulfikri.ac.id>
2022-04-17 00:41:49 +0700

    first commit

---
+++ blog/application/controllers/Mahasiswa.php
@@ -0,0 +1,36 @@
+<?php 
+
+    class Mahasiswa extends CI_Controller{
+        public function index(){
+            //load atau memanggil model mahasiswa
+            $this->load->model('mahasiswa_model','mhs1');
+
+            //buat object model 1 dan nilai 
+            $this->mhs1->id= 1;
+            $this->mhs1->nim='01001';
+            $this->mhs1->nama='Faiz Zikri';
+            $this->mhs1->gender='L';
+            $this->mhs1->ipk=3.85;
+
+            // load atau memanggil model mahasiswa 
+            $this->load->model('mahasiswa_model','mhs2');
+
+            // buat object model 2 dan nilai
+            $this->mhs2->id= 2;
+            $this->mhs2->nim='01222';
+            $this->mhs2->nama='Dinda';
+            $this->mhs2->gender='P';
+            $this->mhs2->ipk=3.55;
+
+            // simpan object yang kita buat tadi ke dalam array
+            $list_mhs = [$this->mhs1, $this->mhs2];
+
+            //simpan data untuk dikirim kedalam view, dimana datanya di ambil dari object yang kita simpan ke dalam array
+            $data['list_mhs'] = $list_mhs;
+
+            // selanjutnya tinggal kita render data ke dalam view
+            $this->load->view('mahasiswa/index', $data);
+        }
+    }
+
+?>
---
+++ blog/application/controllers/Matakuliah.php
@@ -0,0 +1,40 @@
+<?php 
+
+class Matakuliah extends CI_Controller{
+    public function index(){
+        //load atau memanggil model mata kuliah
+        $this->load->model('matakuliah_model','mk1');
+
+        //buat object model 1 dan nilai
+        $this->mk1->nama='Pemrograman Web 2';
+        $this->mk1->sks=3;
+        $this->mk1->kode='Pw2';
+        
+        //load atau memanggil model mata kuliah
+        $this->load->model('matakuliah_model','mk2');
+
+        //buat object model 2 dan nilai
+        $this->mk2->nama='Basis Data';
+        $this->mk2->sks=4;
+        $this->mk2->kode='Basdat';
+
+        //load atau memanggil model mata kuliah
+        $this->load->model('matakuliah_model','mk3');
+
+        //buat object model 3 dan nilai
+        $this->mk3->nama='Jaringan Komputer';
+        $this->mk3->sks=2;
+        $this->mk3->kode='Jarkom';
+
+        // simpan object yang kita buat tadi ke dalam array
+        $list_mk = [$this->mk1, $this->mk2, $this->mk3];
+
+        //simpan data untuk dikirim kedalam view, dimana datanya di ambil dari object yang kita simpan ke dalam array
+        $data['list_mk'] = $list_mk;
+
+        // selanjutnya tinggal kita render data ke dalam view
+        $this->load->view('matakuliah/index', $data);
+    }
+}
+
+?>
